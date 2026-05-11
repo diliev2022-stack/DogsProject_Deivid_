48c0f68360f9dd72b0990d6f41933108ca06cd14
diliev2022-stack <d.iliev2022@pmg-pernik.com>
2026-05-11 09:58:35 +0300

    Initial Commit

---
+++ DogsApp/Models/Dog/DogDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DogsApp.Models.Dog
+{
+    public class DogDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;   
+        public int Age { get; set; }
+
+        public string BreedName { get; set; }
+
+        public string? Picture { get; set; }
+
+        public string FullName { get; set; } = null!;
+    }
+}
